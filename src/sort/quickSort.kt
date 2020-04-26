26540cd0f16dcec64fd19d769399d60c1b4c18b2
song <0603yang@naver.com>
2020-04-26 23:57:08 +0900

    정렬) 퀵소트

--- src/sort/quickSort.kt
+++ src/sort/quickSort.kt
@@ -1,2 +1,16 @@
-package sort
+fun quicksort(items:List<Int>):List<Int>{
+    if (items.count() < 2){
+        return items
+    }
+    val pivot = items[items.count()/2]
+    val equal = items.filter { it == pivot }
+    val less = items.filter { it < pivot }
+    val greater = items.filter { it > pivot }
 
+    return quicksort(less) + equal + quicksort(greater)
+}
+fun main() {
+    val numbers = listOf<Int>(2, 8, 7, 3, 6, 9, 5, 1, 0)
+    val ordered =  quicksort(numbers)
+    println(ordered)
+}
